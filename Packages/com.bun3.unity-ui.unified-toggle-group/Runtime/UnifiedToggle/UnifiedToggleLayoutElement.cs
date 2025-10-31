a09aed9cccaf2da8cf36deb9d244a36350a6ff7d
Bun3 <Bun3@users.noreply.github.com>
2025-10-31 17:24:21 +0900

    add LayoutElement Toggle

---
+++ Packages/com.bun3.unity-ui.unified-toggle-group/Runtime/UnifiedToggle/UnifiedToggleLayoutElement.cs
@@ -0,0 +1,121 @@
+using System;
+using Core.Attributes.ReadOnly;
+using Core.Attributes.Required;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class UnifiedOptionLayoutElementIgnoreLayout : UnifiedOption<LayoutElement, bool>
+{
+    protected override void SetOption(LayoutElement component, bool value)
+    {
+        component.ignoreLayout = value;
+    }
+}
+
+[Serializable]
+public class UnifiedOptionLayoutElementMinWidth : UnifiedOption<LayoutElement, float>
+{
+    protected override float GetDefaultOption()
+    {
+        return -1f;
+    }
+
+    protected override void SetOption(LayoutElement component, float value)
+    {
+        component.minWidth = value;
+    }
+}
+
+[Serializable]
+public class UnifiedOptionLayoutElementMinHeight : UnifiedOption<LayoutElement, float>
+{
+    protected override float GetDefaultOption()
+    {
+        return -1f;
+    }
+
+    protected override void SetOption(LayoutElement component, float value)
+    {
+        component.minHeight = value;
+    }
+}
+
+[Serializable]
+public class UnifiedOptionLayoutElementPreferredWidth : UnifiedOption<LayoutElement, float>
+{
+    protected override float GetDefaultOption()
+    {
+        return -1f;
+    }
+
+    protected override void SetOption(LayoutElement component, float value)
+    {
+        component.preferredWidth = value;
+    }
+}
+
+[Serializable]
+public class UnifiedOptionLayoutElementPreferredHeight : UnifiedOption<LayoutElement, float>
+{
+    protected override float GetDefaultOption()
+    {
+        return -1f;
+    }
+
+    protected override void SetOption(LayoutElement component, float value)
+    {
+        component.preferredHeight = value;
+    }
+}
+
+[Serializable]
+public class UnifiedOptionLayoutElementFlexibleWidth : UnifiedOption<LayoutElement, float>
+{
+    protected override float GetDefaultOption()
+    {
+        return -1f;
+    }
+
+    protected override void SetOption(LayoutElement component, float value)
+    {
+        component.flexibleWidth = value;
+    }
+}
+
+[Serializable]
+public class UnifiedOptionLayoutElementFlexibleHeight : UnifiedOption<LayoutElement, float>
+{
+    protected override float GetDefaultOption()
+    {
+        return -1f;
+    }
+
+    protected override void SetOption(LayoutElement component, float value)
+    {
+        component.flexibleHeight = value;
+    }
+}
+
+[Serializable]
+public class UnifiedOptionLayoutElementLayoutPriority : UnifiedOption<LayoutElement, int>
+{
+    protected override int GetDefaultOption()
+    {
+        return 1;
+    }
+
+    protected override void SetOption(LayoutElement component, int value)
+    {
+        component.layoutPriority = value;
+    }
+}
+
+[RequireComponent(typeof(LayoutElement))]
+public class UnifiedToggleLayoutElement : BaseUnifiedToggle<LayoutElement>
+{
+    [SerializeField, Required(inAutoCache: true), ReadOnly]
+    protected LayoutElement _layoutElement;
+
+    protected override LayoutElement component => _layoutElement;
+}
